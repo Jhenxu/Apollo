e1cb2ad5772c0c814784fe06a620c1af26352f3b
Jhenxu <jhenxu@gmail.com>
2016-01-07 11:38:11 +0800

    add mkdir logs

--- ApolloSpider/spiders/__init__.py
+++ ApolloSpider/spiders/__init__.py
@@ -4,10 +4,13 @@
 # your spiders.
 
 import logging
+import os
 import time
 from scrapy.log import ScrapyFileLogObserver
 
 def logging_start(spider):
+    if not os.path.exists('./logs/'):
+        os.makedirs('./logs/')
     log = 'logs/%s_%s.log' % (time.strftime('%Y-%m-%d_%H:%M:%S',time.localtime(int(time.time()))),spider)
     logfile = open(log, 'w')
     log_observer = ScrapyFileLogObserver(logfile, level=logging.INFO)
